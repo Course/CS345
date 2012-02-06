97eda1daf359a5f77b57780ec71a6a2eb6215017
satvikc <mystic.satvik@gmail.com>
2012-02-07 00:07:41 +0530

    Test cases generator added

---
+++ P1/generateTests.py
@@ -0,0 +1,105 @@
+import random,sys
+
+def random_data_generator (max_r):
+    for i in range(max_r):
+        yield random.randint(0, max_r)
+
+def random_select(l,p):
+    t = [] 
+    for i,v in enumerate(l):
+        t=t+[v]*int(p[i]*100)
+    return random.choice(t)
+def findEnd(d,key):
+    while(key in d):
+        key=d[key]
+    return key 
+# given a dict and a key u which is a node finds randomly a node v such that there is a path from u to v 
+def choosePath(d,key):
+    l = []
+    while key in d:
+        key=d[key]
+        l.append(key)
+    return random.choice(l)
+
+def path(d,key):
+    l = []
+    while key in d:
+        l.append(key)
+        key=d[key]
+    return l
+
+def revPath(din,dout,key):
+    l = []
+    key=findEnd(dout,key)
+    while key in din:
+        l.append(key)
+        oldkey=key
+        key=din[key]
+        din.pop(oldkey)
+        dout.pop(key)
+    l.append(key)
+    for m,n in zip(l,l[1:]):
+        dout[m]=n
+        din[n]=m
+
+if __name__ == "__main__":
+    operations = ['L','C','A','R','M','I']
+    probability = [0.5,0.3,0.05,0.05,0.05,0.05]
+    tnum = 1000000
+    maxweight = 1000
+    maxd = 20
+    testcases = 10000  ## Can be less than 1000 due to continue skips 
+    testfile = 'testcases.txt'
+    edgein = {}
+    edgeout = {}
+    f = open(testfile,'w')
+    f.write(str(tnum)+"\n")
+    for i in range(testcases):    
+        assert(set(edgein.keys())==set(edgeout.values()))
+        assert(set(edgeout.keys())==set(edgein.values()))
+        assert(None not in edgein and None not in edgeout)
+        op=random_select(operations,probability)
+        d = random.randint(-maxd,maxd)
+        print("Generating test",i," with "+op)
+        if op=='L':
+            node1 = random.randint(1,tnum-1)
+            node2 = random.randint(1,tnum-1)
+            weight = random.randint(0,maxweight-1)
+            t1=findEnd(edgein,node1)
+            t2=findEnd(edgeout,node2)
+            edgein[t1]=t2
+            edgeout[t2]=t1
+            f.write("L "+str(node1) + " " +str(node2) + " " + str(weight)+"\n")
+        elif op=='C':
+            if edgein=={}:
+                continue
+            node1=random.choice(list(edgein.keys()))
+            node2=edgein[node1]
+            edgein.pop(node1)
+            edgeout.pop(node2)
+            f.write("C "+str(node1) + " " +str(node2)+"\n")
+        elif op=='A':
+            if edgein=={}:
+                continue
+            node1=random.choice(list(edgein.keys()))
+            node2 = choosePath(edgein,node1)
+            d = random.randint(-maxd,maxd)
+            f.write("M "+str(node1) + " " +str(node2) + " " + str(d)+"\n")
+        elif op=='R':
+            pass
+            if edgein=={}:
+                continue
+            node1=random.choice(list(edgein.keys()))
+            revPath(edgein,edgeout,node1)
+            f.write("R "+str(node1)+"\n")
+        elif op=='M':
+            if edgein=={}:
+                continue
+            node1=random.choice(list(edgein.keys()))
+            node2 = choosePath(edgein,node1)
+            f.write("M "+str(node1) + " " +str(node2)+"\n")
+        else :
+            node1 = random.randint(1,tnum)
+            node2 = random.randint(1,tnum)
+            f.write("I "+str(node1) + " " +str(node2)+"\n")
+
