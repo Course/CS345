7a6b58ca5d65f2510e5108f33faf9bfef57245e4
satvikc <mystic.satvik@gmail.com>
2012-02-15 18:58:34 +0530

    generate test bug fixed

--- P1/generateTests.py
+++ P1/generateTests.py
@@ -1,4 +1,4 @@
-import random,sys
+import random,sys,time
 
 def random_data_generator (max_r):
     for i in range(max_r):
@@ -44,23 +44,24 @@ def revPath(din,dout,key):
 
 if __name__ == "__main__":
     operations = ['L','C','A','R','M','I']
-    probability = [0.9,0,0,0.1,0,0] ## Respective probabilities with which to choose these 
-    tnum = 50                              ## Number of nodes 
+    probability = [0.8,0,0,0.2,0,0] ## Respective probabilities with which to choose these 
+    tnum = 100                              ## Number of nodes 
     maxweight = 1000                            ## Max weight to be used in link 
     maxd = 20                                   ## Argument of multiadd
-    testcases = 25                          ## Can be less than this number due to continue skips 
+    testcases = 100                           ## Can be less than this number due to continue skips 
     testfile = 'testcases.txt'                  ## Test case stored here 
     edgein = {}
     edgeout = {}
-    f = open(testfile,'w')
+    f = open(str(testcases)+testfile,'w')
     f.write(str(tnum)+"\n")
     for i in range(testcases):    
-        assert(set(edgein.keys())==set(edgeout.values()))
-        assert(set(edgeout.keys())==set(edgein.values()))
-        assert(None not in edgein and None not in edgeout)
+        #assert(set(edgein.keys())==set(edgeout.values()))
+        #assert(set(edgeout.keys())==set(edgein.values()))
+        #assert(None not in edgein and None not in edgeout)
         op=random_select(operations,probability)
         d = random.randint(-maxd,maxd)
-        print("Generating test",i," with "+op)
+        if i%1000==0:
+            print("Generating test",i," with "+op)
         if op=='L':
             node1 = random.randint(1,tnum-1)
             node2 = random.randint(1,tnum-1)
@@ -68,6 +69,9 @@ if __name__ == "__main__":
                 continue
             weight = random.randint(0,maxweight-1)
             t1=findEnd(edgein,node1)
+            temp1=findEnd(edgein,node2)
+            if t1==temp1 :
+                continue
             t2=findEnd(edgeout,node2)
             edgein[t1]=t2
             edgeout[t2]=t1
@@ -104,4 +108,3 @@ if __name__ == "__main__":
             node1 = random.randint(1,tnum)
             node2 = random.randint(1,tnum)
             f.write("I "+str(node1) + " " +str(node2)+"\n")
-
